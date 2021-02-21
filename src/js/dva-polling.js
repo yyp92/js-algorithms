a0e23d400cde34c9a6846b8e7f95443e9ffc2509
yangyipeng <yangyipeng@tianyancha.com>
2021-02-21 20:37:39 +0800

    feat: dva-轮询代码-格式化

--- src/js/dva-polling.js
+++ src/js/dva-polling.js
@@ -8,27 +8,27 @@ function * getDvaPolling({ payload }, { put }) {
         // 从另一个modal获取数据
         const {user} = yield select(state => state.user)
 
-      // 轮询的原理来实现
-      // 延时器
-      const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+        // 轮询的原理来实现
+        // 延时器
+        const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms))
 
-      // 用data判断是否还需要查询
-      const check = (data) => {
-        if (user) {
-          // 操作拿到数据的操作
-          
-          return false
+        // 用data判断是否还需要查询
+        const check = (data) => {
+            if (user) {
+            // 操作拿到数据的操作
+            
+            return false
+            }
+
+            return true
         }
 
-        return true
-      }
+        const neeedToCheckAgain = check(user)
 
-      const neeedToCheckAgain = check(user)
+        if (neeedToCheckAgain) {
+            yield delay(500)
 
-      if (neeedToCheckAgain) {
-        yield delay(500)
-
-        // 调用自身
-        yield put({type: 'getDvaPolling'})
-      }
+            // 调用自身
+            yield put({type: 'getDvaPolling'})
+        }
 }
